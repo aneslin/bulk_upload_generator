3f18a275dc65946ecbdb811c96033f0cc89491ec
Brandon Stewart <bstewart40@gsu.edu>
2018-05-01 13:23:43 -0400

    add initial test concept and some helper methods

--- test/bulk_upload_generator_test.rb
+++ test/bulk_upload_generator_test.rb
@@ -7,6 +7,17 @@ class BulkUploadGeneratorTest < MiniTest::Test
 
 	generate_test_app
 
+	copy_fixtures
+	copy_generator
+
+	def test_it_creates_service
+		generate_bulk_uploader
+		
+		assert_file 'app/services/photo_service.rb' do |content|
+	    assert_match('fixtures/services/photo_service.rb', content)
+	  end
+	end
+
 	Minitest.after_run do
 		remove_test_app
 	end
--- test/generator_test_helpers.rb
+++ test/generator_test_helpers.rb
@@ -4,6 +4,16 @@ module GeneratorTestHelpers
 		system "rails new rails_test_app --skip-active-record --skip-test-unit --skip-spring --skip-bundle"
 	end
 
+	def copy_fixtures
+		system 'cp -a test/fixtures/controllers/. rails_test_app/app/controllers/'
+		system 'cp -a test/fixtures/models/. rails_test_app/app/models/'
+	end
+
+	def copy_generator
+		system 'mkdir -p rails_test_app/lib/hyrax/bulk_upload'
+		system 'cp lib/hyrax/bulk_upload/bulk_upload_generator.rb rails_test_app/lib/hyrax/bulk_upload/bulk_upload_generator.rb'
+	end
+
 	def remove_test_app
 		puts 'Deleting rails test app...'
 		system "rm -rf rails_test_app"
@@ -11,6 +21,8 @@ module GeneratorTestHelpers
 
 	def generate_bulk_uploader
 		puts 'Running rails g hyrax:bulk_upload photo'
+		puts 'Running rails g hyrax:bulk_upload oral_history'
 		system "rails g hyrax:bulk_upload photo"
+		system "rails g hyrax:bulk_upload oral_history"
 	end
 end
